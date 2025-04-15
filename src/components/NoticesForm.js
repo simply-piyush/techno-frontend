026d611a53f41cd4a9deea6e1c985e99fccfb81b
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-16 01:46:16 +0530

    Update NoticesForm.js

--- src/components/NoticesForm.js
+++ src/components/NoticesForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+import "../App.css";
 
 export default function NoticesForm() {
   const [form, setForm] = useState({ title: "", description: "" });
