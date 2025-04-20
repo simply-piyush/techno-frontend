f6eea6d355208f300bc31df8a24674df936e4ce5
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-21 03:31:44 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/DashboardLayout.css';
+import '../styles/Dashboard.css';
 
 const DashboardLayout = ({ children }) => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
