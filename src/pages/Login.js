94f1a0609f2b75f631bf37bd002f9c74e16c51d7
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-20 14:08:58 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const BACKEND_URL = 'https://techno-backend.onrender.com'; // replace with your backend URL if different
+const BACKEND_URL = 'https://techno-backend-76p3.onrender.com'; // replace with your backend URL if different
 
 const Login = () => {
   const [roll, setRoll] = useState('');
