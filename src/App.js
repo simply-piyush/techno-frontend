e3a97547d7dbbdbb7c75112fdce3e2826d2003fa
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-20 14:55:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
+import StudentDashboard from "./pages/StudentDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/dashboard" element={<StudentDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
