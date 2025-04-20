76730db23397446786e018d8210cbde93c25abec
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-20 13:26:49 +0530

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,13 +1,13 @@
-const API_BASE_URL = "https://techno-backend.onrender.com/api"; // your real Render URL
+const API_BASE_URL = "https://techno-backend-76p3.onrender.com"; 
 
-export const loginStudent = async ({ studentId, dob }) => {
+export const loginStudent = async ({ roll, password }) => {
   try {
     const response = await fetch(`${API_BASE_URL}/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ studentId, dob }),
+      body: JSON.stringify({ roll, password }),
     });
 
     const data = await response.json();
