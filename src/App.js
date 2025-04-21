b2df65c89361e165d69ac911bfe3404acf243cf0
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-21 10:10:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Login from "./pages/Login";
 import CreateAccount from "./pages/CreateAccount";
 import StudentDashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
+import Home from "./pages/Home"; // ✅ import the new page
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/" element={<Home />} /> {/* Set Home as default */}
+        <Route path="/home" element={<Home />} 
         <Route path="/dashboard" element={<StudentDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
